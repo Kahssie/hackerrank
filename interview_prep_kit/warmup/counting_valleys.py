7351fa60669ff1fa4e76249ac952b8ddff90949c
Cassie Ang <cassie_ang@mymail.sutd.edu.sg>
2022-02-12 02:43:54 +0800

    update counting valleys

---
+++ interview_prep_kit/warmup/counting_valleys.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    valleyCount = 0
+    currentHeight = 0
+    previousHeight = 0
+    
+    for i in range(steps):
+        previousHeight = currentHeight
+        if (path[i]=='U'):
+            currentHeight+=1
+        else:
+            currentHeight-=1
+        
+        if (previousHeight==-1) & (currentHeight==0):
+            valleyCount+=1
+    
+    return valleyCount
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
